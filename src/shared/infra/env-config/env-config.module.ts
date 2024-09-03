436ab00a9ed75316e8dd391eddcc8a859fecf05b
Duarte <jose.duarte@roraimaenergia.com.br>
2024-09-02 20:00:49 -0400

    refactor: adding users resources

--- src/shared/infra/env-config/env-config.module.ts
+++ src/shared/infra/env-config/env-config.module.ts
@@ -1,13 +1,15 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { EnvConfigService } from './env-config.service';
 import { ConfigModule, ConfigModuleOptions } from '@nestjs/config';
-import { join } from 'path';
+import { join } from 'node:path';
 
 @Module({
+  imports: [ConfigModule],
   providers: [EnvConfigService],
+  exports: [EnvConfigService],
 })
 export class EnvConfigModule extends ConfigModule {
-  static forRoot(options?: ConfigModuleOptions): DynamicModule {
+  static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
       return super.forRoot({
         ...options,
         envFilePath: [join(__dirname, `../../../../.env.${process.env.NODE_ENV}`)]
