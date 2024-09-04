1fdb237b96d76c8deb222db3dd6a7ffb085580ae
Duarte <jose.duarte@roraimaenergia.com.br>
2024-09-03 21:05:51 -0400

    feat: add abstract entity class

---
+++ src/shared/domain/entities/__tests__/unit/entity.spec.ts
@@ -0,0 +1,38 @@
+import { validate as uuidValidate } from 'uuid'
+import { Entity } from '../../entity'
+
+type EntityProps = {
+  prop1: string
+  prop2: number
+}
+
+class StubEntity extends Entity<EntityProps> {}
+describe('Abstract Entity unit tests', () => {
+  it('Should be set props and id', () => {
+    const props = { prop1: 'value1', prop2: 15 }
+    const entity = new StubEntity(props)
+
+    expect(entity.props).toStrictEqual(props)
+    expect(entity._id).not.toBeNull()
+    expect(uuidValidate(entity._id)).toBeTruthy()
+  })
+
+  it('Should accept a valid uuid', () => {
+    const props = { prop1: 'value1', prop2: 15 }
+    const id = '3e1a055e-7715-4ed5-b91a-a53f483265fb'
+    const entity = new StubEntity(props, id)
+    console.log('entity', entity._id)
+    expect(uuidValidate(entity._id)).toBeTruthy()
+    expect(entity._id).toEqual(id)
+  })
+
+  it('Should be create a json object', () => {
+    const props = { prop1: 'value1', prop2: 15 }
+    const id = '3e1a055e-7715-4ed5-b91a-a53f483265fb'
+    const entity = new StubEntity(props, id)
+    expect(entity.toJson()).toStrictEqual({
+      id,
+      ...props,
+    })
+  })
+})
