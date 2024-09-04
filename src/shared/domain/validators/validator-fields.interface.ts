af96cc339bcd29a998f2168c6da9ae3ce464682d
duarte <duartydev@gmail.com>
2024-09-04 11:57:38 -0400

    adding ValidatorFieldsInterface

---
+++ src/shared/domain/validators/validator-fields.interface.ts
@@ -0,0 +1,9 @@
+export type FieldsErrors = {
+    [field:string]: Array<string>
+}
+
+export interface ValidatorFieldsInterface<PropsValidated> {
+    errors: FieldsErrors
+    validatedData: PropsValidated
+    validate(data: any): boolean 
+}
