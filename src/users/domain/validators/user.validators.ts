c8d6eb3cefbc42c364c805b36d8dd8b57bb683fa
Duarte <duartydev@gmail.com>
2024-09-09 21:16:38 -0400

    feat: add UserValidatorFactory

--- src/users/domain/validators/user.validators.ts
+++ src/users/domain/validators/user.validators.ts
@@ -1,1 +1,48 @@
-export {}
+import { ClassValidatorFields } from '@/shared/domain/validators/class-validator-fields'
+import {
+  IsDate,
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator'
+import { UserProps } from '../entities/user.entity'
+
+export class UserRules {
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  name: string
+
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string
+
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  password: string
+
+  @IsDate()
+  @IsOptional()
+  createdAt?: Date
+
+  constructor({ name, password, email, createdAt }: UserProps) {
+    Object.assign(this, { name, password, email, createdAt })
+  }
+}
+
+export class UserValidator extends ClassValidatorFields<UserRules> {
+  validate(data: UserProps): boolean {
+    return super.validate(new UserRules(data ?? ({} as UserProps)))
+  }
+}
+
+export class UserValidatorFactory {
+  static create(): UserValidator {
+    return new UserValidator()
+  }
+}
