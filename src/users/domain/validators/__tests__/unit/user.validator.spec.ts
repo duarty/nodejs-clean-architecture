447b54bea5d2f8b71bbff157c0f7777d25206a92
Duarte <duartydev@gmail.com>
2024-09-10 22:49:24 -0400

    feat: add more test invalidation name field cases

--- src/users/domain/validators/__tests__/unit/user.validator.spec.ts
+++ src/users/domain/validators/__tests__/unit/user.validator.spec.ts
@@ -1,3 +1,4 @@
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
 import { UserValidator, UserValidatorFactory } from '../../user.validators'
 
 let sut: UserValidator
@@ -7,8 +8,17 @@ describe('UserValidator unit tests', () => {
   })
   describe('Name field', () => {
     it('Invalidation cases for name field', () => {
-      const isValid = sut.validate(null)
+      let isValid = sut.validate(null)
       expect(isValid).toBeFalsy()
+      expect(sut.errors['name']).toStrictEqual([
+        'name should not be empty',
+        'name must be a string',
+        'name must be shorter than or equal to 255 characters',
+      ])
+
+      isValid = sut.validate({ ...UserDataBuilder({}), name: '' })
+      expect(isValid).toBeFalsy()
+      expect(sut.errors['name']).toStrictEqual(['name should not be empty'])
     })
   })
 })
